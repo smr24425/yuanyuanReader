0e0b001ba856fc194e0be893a0ecaa56b7896c2f
Arthur <smr24425@gmail.com>
2025-10-16 14:41:51 +0800

    creaete all rights reserved

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,22 @@ import BookList from "./components/BookList";
 import { SafeArea } from "antd-mobile";
 
 const App = () => (
-  <>
+  <div style={{ display: "flex", flexDirection: "column", minHeight: "100vh" }}>
     <SafeArea position="top" />
     <BookList />
+    <footer
+      style={{
+        background: "#f7f7f7",
+        textAlign: "center",
+        padding: 16,
+        color: "#000",
+        fontSize: 12,
+      }}
+    >
+      © {new Date().getFullYear()} smr24425. All rights reserved.
+    </footer>
     <SafeArea position="bottom" />
-  </>
+  </div>
 );
 
 export default App;
