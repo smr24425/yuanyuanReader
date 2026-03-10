585501e81a828fdcb03aabf614768bb9da9bb701
smr24425 <smr24425@gmail.com>
2026-03-10 17:31:50 +0800

    fixed pwa.ts

--- src/utils/pwa.ts
+++ src/utils/pwa.ts
@@ -2,14 +2,21 @@ import { registerSW } from "virtual:pwa-register";
 import { Dialog } from "antd-mobile";
 
 export const updateSW = registerSW({
+  // 設定為手動立即更新，而非自動。這能防止背景靜默更新。
+  immediate: false,
+
   onNeedRefresh() {
     Dialog.confirm({
+      title: "版本更新",
       content: "發現新版本，是否立即更新？",
-      confirmText: "立即更新",
-      onConfirm: () => updateSW(true),
+      confirmText: "更新",
+      cancelText: "稍後",
+      onConfirm: () => {
+        updateSW(true);
+      },
+      onCancel: () => {
+        console.log("使用者選擇延遲更新");
+      },
     });
-  },
-  onOfflineReady() {
-    console.log("App 已可離線使用");
   },
 });
